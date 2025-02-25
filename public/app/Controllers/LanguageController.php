36202421770bfc19a0aa3f7776018c16453f28b0
Humayun Hasimler <hmyn@hotmail.com>
2025-02-25 15:15:10 +0100

    code 015

--- public/app/Controllers/LanguageController.php
+++ public/app/Controllers/LanguageController.php
@@ -5,7 +5,7 @@ use CodeIgniter\Controller;
 
 class LanguageController extends Controller
 {
-    public function change($lang)
+   /* public function change($lang)
     {
         $session = session();
         $request = service('request'); // Mevcut URL'yi almak için
@@ -20,5 +20,20 @@ class LanguageController extends Controller
         // Kullanıcının geldiği sayfaya yönlendir
         $referer = $request->getServer('HTTP_REFERER') ?? base_url();
         return redirect()->to($referer);
+    }*/
+    public function change($lang)
+    {
+        $session = session();
+        $availableLanguages = ['tr', 'en', 'de'];
+    
+        if (in_array($lang, $availableLanguages)) {
+            $session->set('site_lang', $lang);
+        }
+    
+        // Session içeriğini log dosyasına yazdıralım
+        log_message('debug', 'Dil değiştirildi: ' . $session->get('site_lang'));
+    
+        return redirect()->back();
     }
+    
 }
