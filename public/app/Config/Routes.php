a8bf0c0f324b421c839116b3669beb36a6c33bdb
Humayun Hasimler <hmyn@hotmail.com>
2025-02-26 16:42:11 +0100

    code Lang degisimi 019

--- public/app/Config/Routes.php
+++ public/app/Config/Routes.php
@@ -13,6 +13,8 @@ $routes->get('admin/logout', 'AdminController::logout');
 $routes->get('admin/dashboard', 'AdminController::dashboard');
 $routes->get('lang/(:segment)', 'LanguageController::change/$1');
 $routes->get('admin/test_session', 'AdminController::testSession');
+$routes->get('/test_headers_sent', 'TestController::testHeadersSent');
+
 
 
 
