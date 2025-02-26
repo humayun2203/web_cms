1827fbd09663d6c6d40bcd31c047225b747091f6
Humayun Hasimler <hmyn@hotmail.com>
2025-02-26 15:39:10 +0100

    code Lang degisimi 014

--- public/test_session.php
+++ public/test_session.php
@@ -1,5 +1,16 @@
 <?php
+// Session çerez ayarlarını düzelt
+session_set_cookie_params([
+    'lifetime' => 86400,  // 1 Gün
+    'path' => '/',
+    'domain' => '',  // Eğer subdomain kullanıyorsan buraya ana domaini yaz
+    'secure' => false,  // HTTPS kullanıyorsan true yap
+    'httponly' => true,
+    'samesite' => 'Lax'
+]);
+
 session_start();
 $_SESSION['test'] = 'Session Çalışıyor!';
+
 echo 'Session ID: ' . session_id();
 ?>
