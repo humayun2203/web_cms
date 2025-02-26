11aa0f3e1ab50d8b3df321c4bc88938e2daa69b4
Humayun Hasimler <hmyn@hotmail.com>
2025-02-26 15:54:18 +0100

    code Lang degisimi 016

---
+++ public/test_headers_sent.php
@@ -0,0 +1,8 @@
+<?php
+if (headers_sent($file, $line)) {
+    echo "Headers already sent in $file on line $line";
+} else {
+    header("Set-Cookie: my_test_cookie=12345; Path=/; HttpOnly; SameSite=Lax");
+    echo "Headers Sent Successfully!";
+}
+?>
